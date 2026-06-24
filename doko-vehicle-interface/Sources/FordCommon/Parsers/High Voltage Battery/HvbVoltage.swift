1ceda4e5db32de849870175b3a726c3545b07285
sillygoose <sillygoose@me.com>
2026-06-24 12:40:45 -0400

    reorganized common folders

---
+++ doko-vehicle-interface/Sources/FordCommon/Parsers/High Voltage Battery/HvbVoltage.swift
@@ -0,0 +1,19 @@
+import VehicleCommon
+import DokoDebug
+
+private struct stpxParser: Parser {
+  var body: some Parser<Substring.UTF8View, Double> {
+    "62480D".utf8
+    UInt16ToDouble()
+  }
+}
+
+public func parseHvbVoltage(_ input: String) throws -> Double {
+#if DEBUG
+  @Shared(.simIdle) var simIdle
+  if simIdle { return 350 }
+#endif
+  var input = input[...].utf8
+  let voltage = try stpxParser().parse(&input)
+  return voltage * 0.01
+}
